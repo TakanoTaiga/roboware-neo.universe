65c8cb3caad0b1e02320c336b5a696737aba7e90
Taiga Takano <ttttghghnb554z@outlook.jp>
2024-04-21 10:51:04 +0900

    change copyright

--- perception/perception_debug/include/perception_debug/bbox2foxglove_node.hpp
+++ perception/perception_debug/include/perception_debug/bbox2foxglove_node.hpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Hakoroboken
+// Copyright 2024 Hakoroboken
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
